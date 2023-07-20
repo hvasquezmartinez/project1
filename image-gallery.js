bcb7cbb3e837be23310924ec90ef2de6b13f24ab
hvasquezmartinez <hvasquezmartinez@student.umgc.edu>
2023-07-19 22:30:36 -0700

    first commit

---
+++ image-gallery.js
@@ -0,0 +1,8 @@
+//Javascript Document
+function myFunction(imgs) {
+    var expandImg = document.getElementById("expandedImg");
+    var imgText = document.getElementById("imgtext");
+    expandImg.src = imgs.src;
+    imgText.innerHTML = imgs.alt;
+    expandImg.parentElement.style.display = "block";
+  }
